d2cbc5d0cea489f37c5233b989b1713858012779
maxlarkin <maxlarkin2007@gmail.com>
2025-06-21 21:26:38 +0400

    google search tools

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,8 @@ export const viewport =  {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        google-site-verification: google83533dcb2b6e513f.html
+      {/*<body className={inter.className}>{children}</body>*/}
     </html>
   )
 }
