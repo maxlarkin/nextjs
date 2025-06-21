c080383a28bf0fd40a224399b9a632c6afcbb2ce
maxlarkin <maxlarkin2007@gmail.com>
2025-06-21 21:43:53 +0400

    google search tools

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,19 @@
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import '../scss/global.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'музей Каменской СОШ',
-  description: 'Оцифрованный музей Каменской СОШ',
-  keywords: ['Каменская СОШ', 'Каменская школа','музей','музей Каменской школы','Каменое', 'Оцифрованный музей'],
-  authors: [
-      {name: 'Larkin Maxim', role: 'main developer'}
+    title: 'музей Каменской СОШ',
+    description: 'Оцифрованный музей Каменской СОШ',
+    keywords: ['Каменская СОШ', 'Каменская школа', 'музей', 'музей Каменской школы', 'Каменое', 'Оцифрованный музей'],
+    authors: [
+        {name: 'Larkin Maxim', role: 'main developer'}
     ],
+    other: {"google-site-verification": "pMNwVdUexTealNEokHfyGlc1XERlk2XXpIP8JbLg4Ew",}
 };
 
-export const viewport =  {
+export const viewport = {
     initialScale: 1.0,
     maximumScale: 1.0,
     minimumScale: 1.0,
@@ -20,11 +21,10 @@ export const viewport =  {
     userScalable: 'no'
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-        google-site-verification: google83533dcb2b6e513f.html
-      {/*<body className={inter.className}>{children}</body>*/}
-    </html>
-  )
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>{children}</body>
+        </html>
+    )
 }
